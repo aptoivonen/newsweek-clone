3456e1b5ee3b7530bc73c895a037c62ee7781d28
Aleksi Toivonen <aptoivonen@mail.com>
2021-01-09 13:08:40 +0200

    Initialize project

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+const purgecss = require("@fullhuman/postcss-purgecss");
+const cssnano = require("cssnano");
+
+module.exports = {
+  plugins: [
+    require("postcss-import"),
+    require("autoprefixer"),
+    cssnano({
+      preset: "default",
+    }),
+    purgecss({
+      content: ["./*.html"],
+      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    }),
+  ],
+};
